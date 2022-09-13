388e24bede3e6cdab303f8218252e70c6a4a49a9
Bamser <bamser@mail.ru>
2022-09-13 18:01:45 +0300

    fixed

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper', {
+    loop: true,
+  
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
